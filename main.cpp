ae4ffcd3e91b8182f58164766aa5a9e7cba475ae
Juraj Kardos <juraj.kardos@usi.ch>
2016-10-03 15:47:59 +0200

    adding debug messages

--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@ using namespace std;
 
 int main(int argv, char* argc[])
 {
-
+        cout << "========================" << endl;
+        cout << "I am about to call MPI-INIT" << endl;
+        cout << "========================" << endl;
     MPI_Init(NULL, NULL);
 
     /* workaround to attach GDB */
-    char hostname[256];
+    char hostname[2048];
     gethostname(hostname, sizeof(hostname));
     printf("Process with PID %d on %s ready to run\n", getpid(), hostname);
     fflush(stdout);
@@ -31,20 +33,22 @@ int main(int argv, char* argc[])
     // if rank == MASTER
     if(mpi_rank == 0)
     {
+        cout << "========================" << endl;
+        cout << "I am about to call MATLAB" << endl;
+        cout << "========================" << endl;
+         //call matlab
+        char comm[500] = "matlab -nosplash -nodisplay -nojvm -nodesktop -r \"interface\"";
+        cout << "Calling the command: " << comm  << " at the MASTER node"<< endl;
+        system(comm);
 
-        //call matlab
-        // char comm[500] = "matlab -nosplash -nodisplay -nojvm -nodesktop -r \"mexsolve\"";
-        // cout << "Calling the command: " << comm  << " at the MASTER node"<< endl;
-        // system(comm);
+        //Engine* ep;
+        //if (!(ep = engOpen("")))
+        //{
+        //    fprintf(stderr, "\n *** Can't start MATLAB engine! *** \n");
+        //    return EXIT_FAILURE;
+        //}
 
-        Engine* ep;
-        if (!(ep = engOpen("")))
-        {
-            fprintf(stderr, "\n *** Can't start MATLAB engine! *** \n");
-            return EXIT_FAILURE;
-        }
-
-        engEvalString(ep, "mexsolve");
+        //engEvalString(ep, "interface");
     }
     else
     {
